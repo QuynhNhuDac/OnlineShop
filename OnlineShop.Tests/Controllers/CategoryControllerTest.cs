6f34b25b11c55b1e76b40a10c7c593e6e1315de0
Hoàng Oanh <hoangoanh9899@gmail.com>
2019-04-06 16:19:29 +0700

    added categoryControllerTest

---
+++ OnlineShop.Tests/Controllers/CategoryControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineShop;
+using OnlineShop.Models;
+using OnlineShop.Controllers;
+
+namespace OnlineShop.Tests.Controllers
+{
+    [TestClass]
+    public class CategoryControllerTest
+    {
+        private CategoryController _categoryCtrl;
+        
+        
+    }
+}
