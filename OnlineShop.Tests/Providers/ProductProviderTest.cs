f3775f586a274a65e2772a5bf2084a63939bce8e
DELL <DELL@DESKTOP-QI4ALMT>
2019-04-14 10:44:59 +0700

    added ProductProviderTest

---
+++ OnlineShop.Tests/Providers/ProductProviderTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineShop.Models;
+using OnlineShop.Provider;
+
+namespace OnlineShop.Tests.Providers
+{
+    [TestClass]
+    public class ProductProviderTest
+    {
+        private Product testProduct;
+        private int testing;
+
+        public ProductProviderTest()
+        {
+            testProduct = new Product();
+            testProduct.ProductName = "Stone Book";
+            testProduct.CategoryID = 1;
+            testProduct.UnitPrice = 500;
+            testProduct.UnitsInStock = 5;
+            testProduct.Discontinued = null;
+            testProduct.Image = null;
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        //[TestMethod]
+        //public void getAllProduct()
+        //{
+        //    //Arrange
+        //    var _provider = new ProductProvider();
+        //    //Act
+        //    var result = _provider.GetProducts().Length;
+        //    //Assert
+        //    Assert.AreEqual(14, result);
+        //}
+
+        [TestMethod]
+        public void CreateProduct()
+        {
+            //Arrange
+            var _provider = new ProductProvider();
+            var name = "Stone Book";
+            //Act
+            var result = Int32.Parse(_provider.Create(testProduct).ToString());
+            var details = _provider.GetDetails(result);
+            //Assert
+            Assert.AreEqual(name, details.ProductName);
+        }
+
+        [TestMethod]
+        public void EditProduct()
+        {
+            //Arrange
+            var _provider = new ProductProvider();
+            //testProduct.ID = 1;
+            //testProduct.ProductName = "Fantasic Beast And Where To Find Them";
+            //testProduct.CategoryID = 1;
+            //testProduct.UnitPrice = 97980;
+            //testProduct.UnitsInStock = 100;
+            //testProduct.Discontinued = false;
+            //testProduct.Image = "~/images/biabenthanhchai.jpg";
+            //Act
+            var result = _provider.Edit(testProduct);
+            //Assert
+            Assert.IsTrue(result, "Can't find this Product!");
+        }
+
+        [TestMethod]
+        public void DeletedProduct()
+        {
+            //Arrange
+            var _provider = new ProductProvider();
+            testProduct.ID = 1;
+            //Act
+            var result = _provider.Delete(testProduct.ID);
+            //Assert
+            Assert.IsTrue(result, "Can't find this Product!");
+        }
+
+
+    }
+}
---
+++ OnlineShop.Tests/Providers/ShipperTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineShop.Models;
+using OnlineShop.Provider;
+
+namespace OnlineShop.Tests.Providers
+{
+    class ShipperTest
+    {
+        private Shipper testShipper;
+        private int testing;
+        public ShipperTest()
+        {
+            testShipper = new Shipper();
+            testShipper.Name = "BiuGates";
+            testShipper.Address = "Ha Tinh - SanHok";
+            testShipper.Phone = 09090909;
+        }
+
+        public int getShipper()
+        {
+            var _provider = new ShipperProvider();
+            testing = _provider.GetShippers().Length;
+            return testing;
+        }
+
+        [TestMethod]
+        public void GetAllShippers()
+        {
+            //Arrange
+            var _provider = new ShipperProvider();
+            getShipper();
+            //Act
+            var result = _provider.GetShippers().Length;
+            var expected = testing;
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void CreateShipper()
+        {
+            //Arrange
+            var _provider = new ShipperProvider();
+            var name = "Oanh";
+            //Act
+            var result = Int32.Parse(_provider.Create(testShipper).ToString());
+            var details = _provider.GetDetails(result);
+            //Assert
+            Assert.AreEqual(name, details.Name);
+        }
+
+        [TestMethod]
+        public void EditShipper()
+        {
+            //Arrange
+            var _provider = new ShipperProvider();
+            testShipper.ID = 1;
+            testShipper.Name = "Sam";
+            testShipper.Address = "SanMartin";
+            //Act
+            var result = _provider.Edit(testShipper);
+            //Assert
+            Assert.IsTrue(result, "Can't find this Shipper!");
+        }
+
+        [TestMethod]
+        public void DeletedShipper()
+        {
+            //Arrange
+            var _provider = new ShipperProvider();
+            testShipper.ID = 1;
+            //Act
+            var result = _provider.Delete(testShipper.ID);
+            //Assert
+            Assert.IsTrue(result, "Can't find this Shipper!");
+        }
+
+        private TestContext test;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return test;
+            }
+            set
+            {
+                test = value;
+            }
+        }
+
+
+        [TestMethod]
+        [DeploymentItem("ShipperTestData.csv")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\ShipperTestData.csv", "ShipperTestData#csv", DataAccessMethod.Sequential)]
+
+        public void GetDetailsByIdData()
+        {
+            //Arrange
+            var _provider = new ShipperProvider();
+            var id = Int32.Parse(TestContext.DataRow[0].ToString());
+            var name = TestContext.DataRow[1].ToString();
+            //Act
+            var result = _provider.GetDetails(id);
+            //Assert
+            Assert.AreEqual(name, result.Name);
+        }
+    }
+}
